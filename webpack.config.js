b1406b1f4d4123356051902242ed1a468f8b84fb
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-18 23:58:53 +0530

    Error: Cannot find module 'webpack/bin/config-yargs'

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,39 @@
-const path = require('path');
-module.exports = 
-{
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractPlugin = new ExtractTextPlugin({
+   filename: 'main.css'
+});
+
+module.exports = {
     entry: './src/js/app.js',
-    output : {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'js/bundle.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
     },
-   devServer:
-   {
-       contentBase: './dist'
-   },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: extractPlugin.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractPlugin
+    ]
 };
